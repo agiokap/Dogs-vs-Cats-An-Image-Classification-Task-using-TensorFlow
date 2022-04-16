fbf31246183ac9ff63df7ab283f321a5a4bc0c26
agiopouloskaptsikas <73758513+agiopouloskaptsikas@users.noreply.github.com>
2022-04-16 10:46:59 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -8,22 +8,32 @@ Created on Fri Apr 15 16:23:53 2022
 import json
 import matplotlib.pyplot as plt
 
-with open(path_to_history_json) as history_json:
-    history = json.load(history_json)
+def load_history(path):
+    with open(path) as history_json:
+        history = json.load(history_json)
+    return history
 
-acc, val_acc = history["accuracy"], history["val_accuracy"]
-loss, val_loss = history["loss"], history["val_loss"]
-
-num_epochs = range(len(acc))
-
-plt.plot(num_epochs, acc)
-plt.plot(num_epochs, val_acc)
-plt.title("Training and validation accuracy")
-plt.legend(["Training", "Validation"])
-plt.figure()
-
-plt.plot(num_epochs, loss)
-plt.plot(num_epochs, val_loss)
-plt.title("Training and validation loss")
-plt.legend(["Training", "Validation"])
-plt.figure()
+def acc_val_plots(history, from_json = False, augmentation = False, transfer_learning = False):
+    
+    if from_json:
+        history = load_history(history)
+    else:
+        acc, val_acc = history["accuracy"], history["val_accuracy"]
+        loss, val_loss = history["loss"], history["val_loss"]
+        
+        num_epochs = range(len(acc))
+        
+        fig, (ax_acc, ax_loss) = plt.subplots(2, 1)
+        fig.suptitle("Accuracy and Loss ({} Augmentation/{} Transfer Learning)".format(("with" if augmentation == True else "without"),
+                                                                                       ("with" if transfer_learning == True else "without")))
+        
+        ax_acc.plot(num_epochs, acc)
+        ax_acc.plot(num_epochs, val_acc)
+        plt.legend(["Training", "Validation"])
+        ax_acc.set_ylabel("Accuracy")
+        
+        ax_loss.plot(num_epochs, loss)
+        ax_loss.plot(num_epochs, val_loss)
+        plt.legend(["Training", "Validation"])
+        ax_loss.set_ylabel("Loss")
+        ax_loss.set_xlabel("Epoch")
