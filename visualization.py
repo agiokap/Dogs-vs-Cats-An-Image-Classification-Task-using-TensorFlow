cf3d46addcec98c119f8cb7bfd8325bfb524d0dd
agiopouloskaptsikas <73758513+agiopouloskaptsikas@users.noreply.github.com>
2022-04-17 09:09:20 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -17,23 +17,22 @@ def acc_val_plots(history, from_json = False, augmentation = False, transfer_lea
     
     if from_json:
         history = load_history(history)
-    else:
-        acc, val_acc = history["accuracy"], history["val_accuracy"]
-        loss, val_loss = history["loss"], history["val_loss"]
-        
-        num_epochs = range(len(acc))
-        
-        fig, (ax_acc, ax_loss) = plt.subplots(2, 1)
-        fig.suptitle("Accuracy and Loss ({} Augmentation/{} Transfer Learning)".format(("with" if augmentation == True else "without"),
-                                                                                       ("with" if transfer_learning == True else "without")))
-        
-        ax_acc.plot(num_epochs, acc)
-        ax_acc.plot(num_epochs, val_acc)
-        plt.legend(["Training", "Validation"])
-        ax_acc.set_ylabel("Accuracy")
-        
-        ax_loss.plot(num_epochs, loss)
-        ax_loss.plot(num_epochs, val_loss)
-        plt.legend(["Training", "Validation"])
-        ax_loss.set_ylabel("Loss")
-        ax_loss.set_xlabel("Epoch")
+
+    acc, val_acc = history["accuracy"], history["val_accuracy"]
+    loss, val_loss = history["loss"], history["val_loss"]
+    num_epochs = range(len(acc))
+
+    fig, (ax_acc, ax_loss) = plt.subplots(2, 1)
+    fig.suptitle("Accuracy and Loss ({} Augmentation/{} Transfer Learning)".format(("with" if augmentation == True else "without"),
+                                                                                   ("with" if transfer_learning == True else "without")))
+
+    ax_acc.plot(num_epochs, acc)
+    ax_acc.plot(num_epochs, val_acc)
+    plt.legend(["Training", "Validation"])
+    ax_acc.set_ylabel("Accuracy")
+
+    ax_loss.plot(num_epochs, loss)
+    ax_loss.plot(num_epochs, val_loss)
+    plt.legend(["Training", "Validation"])
+    ax_loss.set_ylabel("Loss")
+    ax_loss.set_xlabel("Epoch")
